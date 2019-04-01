08dfb5f97bdc49012f4cb08050110986ade27bcd
JPLeon18 <juanpabloleonmera@gmail.com>
2019-04-01 17:05:08 -0500

    Se termina la parte de las cookies

---
+++ 18-formularios/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <form action="" method="post" enctype="multipart/form-data"></form>
+    <h2>Formularios</h2>
+
+    <p>
+    <label for="nombre">Nombre: </label>
+    <input type="text" name="nombre" autofocus>
+    </p>
+    <p>
+        <label for="apellido">Apellido: </label>
+        <input type="text" name="apellido" >
+    </p>
+
+</body>
+</html>
