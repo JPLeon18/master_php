de9389038a26fa847873981b5e43bf946d383461
JPLeon18 <juanpabloleonmera@gmail.com>
2019-07-04 17:13:47 -0500

    creacion de base de datos por sql

--- SQL/03-diseno/blog.sql
+++ SQL/03-diseno/blog.sql
@@ -1,28 +1,29 @@
 CREATE TABLE usuarios(
-id INT (5) AUTO_INCREMENT NOT NULL
+id INT (5) AUTO_INCREMENT NOT NULL,
 nombre VARCHAR (100),
 apellido VARCHAR (100),
 email VARCHAR (100),
 password VARCHAR (50),
 fecha_registro DATE ,
-CONSTRAINT pk_usuarios PRIMARY KEY (id)
-);
+CONSTRAINT pk_usuarios PRIMARY KEY (id),
+CONSTRAINT uq_email UNIQUE (email)
+)ENGINE=InnoDB;
 
 CREATE TABLE categorias(
 id INT (5) AUTO_INCREMENT NOT NULL ,
-nombre VARCHAR (50)
+nombre VARCHAR (50),
 CONSTRAINT pk_categorias PRIMARY KEY (id)
-);
+)ENGINE=InnoDB;
 
 CREATE TABLE entradas(
 id INT (5) AUTO_INCREMENT NOT NULL ,
-usuario_id INT (5),
-categoria_id INT (5),
+usuario_id INT (5) NOT NULL,
+categoria_id INT (5) NOT NULL,
 titulo VARCHAR (100),
-descripcion VARCHAR (255),
+descripcion MEDIUMTEXT,
 fecha DATE ,
 CONSTRAINT pk_entradas PRIMARY KEY (id),
-CONSTRAINT fk_entradas FOREIGN KEY (usuario_id),
-CONSTRAINT fk_entradas FOREIGN KEY (categoria_id)
-);
+CONSTRAINT fk_entrada_usuario FOREIGN KEY (usuario_id) REFERENCES  usuarios(id),
+CONSTRAINT fk_entrada_categoria FOREIGN KEY (categoria_id) REFERENCES categorias(id)
+)ENGINE=InnoDB;
 
