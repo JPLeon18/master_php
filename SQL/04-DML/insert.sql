340a5a4e42ef6c99b5fcb10750694b7b2f91314a
JPLeon18 <juanpabloleonmera@gmail.com>
2019-07-04 21:36:37 -0500

    insert sencillo

---
+++ SQL/04-DML/insert.sql
@@ -0,0 +1,5 @@
+-- Insert en la tabla usuarios --
+
+INSERT INTO usuarios VALUES (null, 'carmen', 'rodolfa', 'carina@juan.com', '123', '2019-01-01');
+INSERT INTO usuarios VALUES (null, 'venito', 'camelas', 'cardona@juan.com', '123', '2019-01-01');
+INSERT INTO usuarios VALUES (null, 'elver gomez', 'torba', 'carto@juan.com', '123', '2019-01-01');
