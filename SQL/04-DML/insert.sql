b4b01a1d6ac6a0c8248c572bbe7bce15f715485d
JPLeon18 <juanpabloleonmera@gmail.com>
2019-07-10 08:13:47 -0500

    Merge branch 'master' of C:\wamp64\www\Master_php with conflicts.

--- SQL/04-DML/insert.sql
+++ SQL/04-DML/insert.sql
@@ -4,6 +4,6 @@ INSERT INTO usuarios VALUES (null, 'carmen', 'rodolfa', 'carina@juan.com', '123'
 INSERT INTO usuarios VALUES (null, 'venito', 'camelas', 'cardona@juan.com', '123', '2019-01-01');
 INSERT INTO usuarios VALUES (null, 'elver gomez', 'torba', 'carto@juan.com', '123', '2019-01-01');
 
--- modificar solo ciertos ampos de un registro--
 
-INSERT INTO usuarios(email, password) VALUES ('juan@juan.com', '123');
+-- Insert filas solo con ciertas columnas --
+
