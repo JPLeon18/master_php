f094bdc73459f339c9411ea8b00caeb389c1b2f3
JPLeon18 <juanpabloleonmera@gmail.com>
2019-07-11 22:50:11 -0500

    Consultas Multitabla

--- SQL/06-subconsultas/consulta.sql
+++ SQL/06-subconsultas/consulta.sql
@@ -12,8 +12,26 @@ SELECT nombre, apellido FROM usuarios WHERE id IN (SELECT usuario_id FROM entrad
 
 SELECT  * FROM entradas WHERE categoria_id IN (SELECT id FROM categorias WHERE nombre = 'Accion');
 
-SELECT * FROM categorias WHERE id IN (SELECT categoria_id FROM entradas WHERE COUNT(id) >= 2);
+SELECT * FROM categorias WHERE id IN (SELECT categoria_id FROM entradas GROUP BY categoria_id HAVING COUNT(categoria_id) >= 2);
 
-SELECT * FROM usuarios WHERE id IN (SELECT usuario_id FROM entradas WHERE WEEKDAY(fecha) = 'Tuesday');
+SELECT * FROM usuarios WHERE id IN (SELECT usuario_id FROM entradas WHERE WEEKDAY(fecha) = 3);
 
+SELECT nombre FROM usuarios WHERE id = (SELECT usuario_id FROM entradas GROUP BY usuario_id ORDER BY COUNT(id) DESC LIMIT 1);
+
+SELECT nombre FROM categorias WHERE id NOT IN (SELECT categoria_id FROM entradas);
+
+
+SELECT entradas.titulo, usuarios.nombre, categorias.nombre
+FROM entradas, usuarios, categorias
+WHERE entradas.usuario_id = usuarios.id AND entradas.categoria_id = categorias.id;
+
+SELECT categorias.nombre, COUNT(entradas.id) AS 'CANTIDAD DE ENTRADAS'
+FROM categorias, entradas
+WHERE entradas.categoria_id = categorias.id
+GROUP BY categorias.nombre;
+
+SELECT usuarios.email, COUNT(entradas.id) AS 'CANTIDAD DE ENTRADAS'
+FROM usuarios, entradas
+WHERE entradas.usuario_id = usuarios.id
+GROUP BY usuarios.id;
 
