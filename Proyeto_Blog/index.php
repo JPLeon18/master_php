da43cba31ddf1c8378e3afcf30e82b815d206f37
JPLeon18 <juanpabloleonmera@gmail.com>
2019-08-06 23:49:49 -0500

    blog

--- Proyeto_Blog/index.php
+++ Proyeto_Blog/index.php
@@ -25,7 +25,10 @@
     <div class="clearfix"></div>
     </header>
 
+    <div class="contenedor">
+
     <aside class="sidebar">
+
         <div class="ingreso">
             <form action="">
                 <label for="correo">Usuario</label>
@@ -37,9 +40,7 @@
                 <input type="submit" value="Entrar">
             </form>
         </div>
-    </aside>
 
-    <aside class="sidebar">
         <div class="registro">
             <form action="">
                 <label for="nombre">nombre</label>
@@ -60,27 +61,41 @@
     </aside>
 
 
-    <section class="contenido">
-        <div>
+    <div class="contenido">
+
+        <h1>Ultimas entradas</h1>
             <article class="articulo">
-                <h1>Fortnite</h1>
+                <a href="">
+                <h2>Fortnite</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipiscing elit enim, sociosqu conubia tortor nullam in eleifend quam suspendisse semper, erat diam montes egestas donec libero id. Velit venenatis fusce sem sapien litora orci curae primis, tempor nibh nisl urna auctor diam luctus, elementum morbi ante mattis consequat inceptos quis. Placerat magna metus phasellus facilisi dui fusce praesent, est augue potenti sem nisi netus, interdum vivamus porta neque senectus egestas.</p>
+                </a>
             </article>
             <article class="articulo">
-                <h1>PES 2020</h1>
+                <a href="">
+                <h2>PES 2020</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipiscing elit enim, sociosqu conubia tortor nullam in eleifend quam suspendisse semper, erat diam montes egestas donec libero id. Velit venenatis fusce sem sapien litora orci curae primis, tempor nibh nisl urna auctor diam luctus, elementum morbi ante mattis consequat inceptos quis. Placerat magna metus phasellus facilisi dui fusce praesent, est augue potenti sem nisi netus, interdum vivamus porta neque senectus egestas.</p>
+                </a>
             </article>
             <article class="articulo">
-                <h1>F1 2020</h1>
+                <a href="">
+                <h2>F1 2020</h2>
                 <p>Suspendisse vehicula tincidunt class vulputate proin lacus himenaeos montes lobortis curae, pretium euismod semper quam vivamus senectus nisl arcu tempor metus commodo, duis facilisi nam sollicitudin porta aliquet at molestie vel. Ullamcorper congue convallis blandit lacinia lacus orci mi dictum placerat, nibh vestibulum nostra porta suspendisse a ornare varius sagittis primis, volutpat mauris cubilia rhoncus accumsan fringilla class nam. Duis hendrerit mattis egestas eleifend quisque cursus ridiculus dignissim eros, senectus conubia class turpis lobortis lacus auctor donec dictum suscipit, a parturient nascetur volutpat vehicula velit nam posuere.</p>
+                </a>
             </article>
+
+        <div class="verTodo">
+            <a href="">Ver todo...</a>
         </div>
-    </section>
+    </div>
+
+    <div class="clearfix"></div>
+
+    </div>
 
 
     <footer>
 
-        <p>Desarrollado por &copy;JPLeon 2019</p>
+        <p class="copy">Desarrollado por &copy;JPLeon 2019</p>
 
     </footer>
 
